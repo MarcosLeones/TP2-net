3858e91664b4003024d9929d307f0b667b5c2a3c
Ignacio Tobajas Ramirez <ignacio_nob_93@hotmail.com>
2022-07-21 23:41:38 -0300

    se añadio Curso adapter y CursoLogic + modificacion web.sitemap

---
+++ Business.Logic/CursoLogic.cs
@@ -0,0 +1,50 @@
+﻿using Business.Entities;
+using Data.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Logic
+{
+    public class CursoLogic : BusinessLogic
+    {
+        private CursoAdapter _CursoData;
+
+        public CursoAdapter CursoData
+        {
+            get { return _CursoData; }
+            set { _CursoData = value; }
+        }
+
+        public CursoLogic()
+        {
+            CursoData = new CursoAdapter();
+        }
+
+        public List<Curso> GetAll()
+        {
+            List<Curso> lista = CursoData.GetAll();
+            return lista;
+        }
+
+        public Curso GetOne(int ID)
+        {
+            return CursoData.GetOne(ID);
+        }
+
+        public void Delete(int ID)
+        {
+            CursoData.Delete(ID);
+        }
+
+        public void Save(Curso curso)
+        {
+            CursoData.Save(curso);
+        }
+    }
+
+}
+
+
---
+++ Data.Database/Data.Database/CursoAdapter.cs
@@ -0,0 +1,197 @@
+﻿using Business.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Database
+{
+    public class CursoAdapter : Adapter
+    {
+
+
+        public List<Curso> GetAll()
+        {
+            List<Curso> cursos = new List<Curso>();
+
+            try
+            {
+                this.OpenConnection();
+
+                SqlCommand cmdCursos = new SqlCommand("select * from cursos", sqlConn);
+
+                SqlDataReader drCursos = cmdCursos.ExecuteReader();
+
+                while (drCursos.Read())
+                {
+                    Curso curso = new Curso();
+
+                    curso.ID = (int)drCursos["id_curso"];
+                    curso.IDMateria = (int)drCursos["id_materia"];
+                    curso.AnioCalendario = (int)drCursos["anio_calendario"];
+                    curso.cupo = (int)drCursos["cupo"];
+
+
+                    cursos.Add(curso);
+                }
+
+                drCursos.Close();
+            }
+
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada =
+               new Exception("Error al recuperar lista de cursos", Ex);
+                throw ExcepcionManejada;
+            }
+
+            finally
+            {
+                this.CloseConnection();
+            }
+
+            return cursos;
+        }
+
+        public Curso GetOne(int ID)
+        {
+            Curso curso = new Curso();
+
+            try
+            {
+                this.OpenConnection();
+
+                SqlCommand cmdCursos = new SqlCommand("select * from comisiones where id_comision=@id", sqlConn);
+                cmdCursos.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                SqlDataReader drCursos = cmdCursos.ExecuteReader();
+
+                if (drCursos.Read())
+                {
+                    curso.ID = (int)drCursos["id_curso"];
+                    curso.IDMateria = (int)drCursos["id_materia"];
+                    curso.AnioCalendario = (int)drCursos["anio_calendario"];
+                    curso.cupo = (int)drCursos["cupo"];
+                }
+
+                drCursos.Close();
+            }
+
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada =
+               new Exception("Error al recuperar curso", Ex);
+                throw ExcepcionManejada;
+            }
+
+            finally
+            {
+                this.CloseConnection();
+            }
+
+            return curso;
+        }
+
+
+        public void Delete(int ID)
+        {
+            try
+            {
+                this.OpenConnection();
+
+                SqlCommand cmdDelete = new SqlCommand("delete cursos where id_curso=@id", sqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al eliminar comision", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Save(Curso curso)
+        {
+
+            if (curso.State == BusinessEntity.States.Deleted)
+            {
+                this.Delete(curso.ID);
+            }
+            else if (curso.State == BusinessEntity.States.New)
+            {
+                this.Insert(curso);
+            }
+            else if (curso.State == BusinessEntity.States.Modified)
+            {
+                this.Update(curso);
+            }
+            curso.State = BusinessEntity.States.Unmodified;
+        }
+
+
+
+
+        protected void Update(Curso curso)
+        {
+            try
+            {
+                this.OpenConnection();
+
+                SqlCommand cmdSave = new SqlCommand("UPDATE cursos set anio_calendario=@anio, id_materia=@mat, id_comision=@com, cupo=@cupo, WHERE id_curso=@id", sqlConn);
+
+                cmdSave.Parameters.Add("@anio", SqlDbType.Int).Value = curso.AnioCalendario;
+                cmdSave.Parameters.Add("@mat", SqlDbType.Int).Value = curso.IDMateria;
+                cmdSave.Parameters.Add("@com", SqlDbType.Int).Value = curso.IDComision;
+                cmdSave.Parameters.Add("@cupo", SqlDbType.Int).Value = curso.cupo;
+
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al modificar datos del curso", ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+
+        }
+
+        protected void Insert(Curso curso)
+        {
+            try
+            {
+                this.OpenConnection();
+
+                SqlCommand cmdSave = new SqlCommand("INSERT INTO cursos (anio_calendario, id_materia, id_comision, cupo) VALUES (@anio, @mat, @com, @cupo) select @@identity", sqlConn);
+
+                 cmdSave.Parameters.Add("@anio", SqlDbType.Int).Value = curso.AnioCalendario;
+                cmdSave.Parameters.Add("@mat", SqlDbType.Int).Value = curso.IDMateria;
+                cmdSave.Parameters.Add("@com", SqlDbType.Int).Value = curso.IDComision;
+                cmdSave.Parameters.Add("@cupo", SqlDbType.Int).Value = curso.cupo;
+
+                curso.ID = Decimal.ToInt32((decimal)cmdSave.ExecuteScalar());
+            }
+            catch (Exception ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al crear comision", ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+
+        }
+
+
+    }
+}
