f3914c19af2b0cf3b9556525c8fc7de4e5791c55
Ignacio Tobajas Ramirez <ignacio_nob_93@hotmail.com>
2022-10-31 17:13:23 -0300

    Añadido export PDF

--- UI.Desktop/ReportesPlanesDesktop.cs
+++ UI.Desktop/ReportesPlanesDesktop.cs
@@ -9,6 +9,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace UI.Desktop
 {
@@ -37,8 +41,10 @@ namespace UI.Desktop
             foreach (var plan in listaPlanes)
             {
                 if (cbReportesPlanes.SelectedItem.ToString() == plan.Descripcion )
-                {
+                {                    
                     this.dgvReportePlanes.DataSource = listaMateriasSinFiltro.Where(lm => lm.IDPlan == plan.ID).ToList();
+                    this.ModificarEstilos();
+                    this.ocultarColumns();
                 }
                 
             }
@@ -69,6 +75,162 @@ namespace UI.Desktop
         private void btnSalir_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void ModificarEstilos() 
+        {
+            
+            for (int numCol = 0; numCol < dgvReportePlanes.Columns.Count; numCol++)
+            {
+                switch (dgvReportePlanes.Columns[numCol].Name)
+                {
+                    case "HSSemanales":
+                        dgvReportePlanes.Columns[numCol].HeaderText = "Horas Semanales";
+                        dgvReportePlanes.Columns[numCol].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                        dgvReportePlanes.Columns[numCol].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                        continue;
+                    case "HSTotales":
+                        dgvReportePlanes.Columns[numCol].HeaderText = "Horas Totales";
+                        dgvReportePlanes.Columns[numCol].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                        dgvReportePlanes.Columns[numCol].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                        continue;
+
+
+                }
+
+
+            }
+        }
+
+        private void ocultarColumns()
+        {
+            for (int numCol = 0; numCol < dgvReportePlanes.Columns.Count; numCol++)
+            {
+                switch (dgvReportePlanes.Columns[numCol].Name)
+                {
+                    case "IDPlan":
+                        dgvReportePlanes.Columns[numCol].Visible = false;
+                        continue;
+                    case "ID":
+                        dgvReportePlanes.Columns[numCol].Visible = false;
+                        continue;
+                    case "State":
+                        dgvReportePlanes.Columns[numCol].Visible = false;
+                        continue;
+
+                }
+
+
+            }
+        }
+
+        private void btnExportPDF_Click(object sender, EventArgs e)
+        {
+            if (dgvReportePlanes.Rows.Count > 0)
+            {
+                SaveFileDialog save = new SaveFileDialog();
+                save.Filter = "PDF (*.pdf)|*.pdf";
+                save.FileName = "Reportes-Planes.pdf";
+                bool errorMessage = false;
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(save.FileName))
+                    {
+                        try
+                        {
+                            File.Delete(save.FileName);
+                        }
+                        catch (Exception ex)
+                        {
+                            errorMessage = true;
+                            MessageBox.Show("No se puede escribir data en el disco" + ex.Message);
+                        }
+                    }
+                    if (!errorMessage)
+                    {
+                        try
+                        {
+
+                            PdfPTable pdfTable = new PdfPTable(dgvReportePlanes.Columns.Count - 3);
+                            pdfTable.DefaultCell.Padding = 2;
+                            pdfTable.WidthPercentage = 100;
+                            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+
+                            
+
+                            foreach (DataGridViewColumn col in dgvReportePlanes.Columns)
+                            {
+                                if (col.Visible)
+                                {
+                                    PdfPCell pdfCell = new PdfPCell(new Phrase(col.HeaderText));
+                                    
+                                    if (col.HeaderText.ToLower() != "descripcion")
+                                    {
+                                        pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                                    }
+                                    
+                                    pdfTable.AddCell(pdfCell);
+                                }
+
+                            }
+
+                            foreach (DataGridViewRow viewRow in dgvReportePlanes.Rows)
+                            {
+                                foreach (DataGridViewCell dgvCell in viewRow.Cells)
+                                {
+                                    if (dgvCell.Visible)
+                                    {
+                                        PdfPCell pdfCell = new PdfPCell(new Phrase(dgvCell.Value.ToString()));
+                                        if (dgvCell.ColumnIndex != 0)
+                                        {
+                                            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                                        }
+                                        pdfTable.AddCell(pdfCell);
+                                    }
+                                    
+                                }
+
+                            }
+
+                            using (FileStream fileStream = new FileStream(save.FileName, FileMode.Create))
+                            {
+                                Document document = new Document(PageSize.A4, 8f, 16f, 16f, 8f);
+
+                                PdfWriter pdfWriter = PdfWriter.GetInstance(document, fileStream);
+                                document.Open();
+                                document.AddAuthor("Acedemia");
+                                document.AddTitle("Reporte de Planes");
+
+                                iTextSharp.text.Font standarFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+
+                                document.Add(new Paragraph("Plan Año: " + cbReportesPlanes.SelectedItem.ToString()));
+                                document.Add(Chunk.NEWLINE);
+                                document.Add(new Paragraph("Materias: "));
+                                document.Add(Chunk.NEWLINE);
+                                document.Add(pdfTable);
+                                document.Close();
+                                fileStream.Close();
+                                pdfWriter.Close();
+
+                            }
+                            MessageBox.Show("La Data se exportó satisfactoriamente", "info");
+
+                        }
+                        catch (Exception)
+                        {
+
+                            MessageBox.Show("Error al guardar");
+                        }
+
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("no se encontró informacion de registro", "info");
+            }
+
+
         }
     }
 }
